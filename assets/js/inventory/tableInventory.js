41d31d5956545c5fe735d37e1475817ea856bb80
DannielVillalvazo <124105143+DannielVillalvazo@users.noreply.github.com>
2025-01-16 10:09:33 -0600

    Cambios
    * Validaciones
    * Se edito el footer del aplicativo

--- assets/js/inventory/tableInventory.js
+++ assets/js/inventory/tableInventory.js
@@ -1,24 +1,33 @@
 var $table = $('#report-table')
 let firstTime = true
+const btnUpdate = document.getElementById("refreshTable");
 
 
 const refreshData = () => {
+    btnUpdate.disabled = true;
+    loading("Cargando...")
     GetItems()
         .then(data => {
+            Swal.close()
              
             if (!data.Success) { 
                 mymessage("error", data.Error.Description)
                 return
             } 
-
+            // Actualizar la tabla
             $table.bootstrapTable("refreshOptions", { data: data.Data })
-
+            // Mandar mensaje de Exito
             firstTime ? "" : mymessage("success", "Informaci\u00f3n actualizada")
-
+            // Cambiar Variable de primera vez
             firstTime = false
+            // Activar el boton
+            btnUpdate.disabled = false;
+
 
         })
-        .catch(error => console.error("Error:", error));
+        .catch(error => {
+            console.error("Error:", error)
+        });
 }
 $(function () { 
 
