98a05384a097c34abe29833f34377f4ab8a49231
DannielVillalvazo <124105143+DannielVillalvazo@users.noreply.github.com>
2025-01-16 09:46:46 -0600

    Nuevo Estilo
    Se añadio el estilo de wrap para la tabla

--- Models/Globals.cs
+++ Models/Globals.cs
@@ -10,13 +10,23 @@ namespace dermaexpressReporte.Models
         //inicialización y crear a valores
         public static Dictionary<string, string> DiccionarioUsuarios = new Dictionary<string, string>()
         {
-            {"ivan.lopez@dermaexpress.com.mx", "Mepiel2025"},
-            {"jesus.herrera@dermaexpress.com.mx", "Mepiel2025"}
+            {"ivan.lopez", "Mepiel2025"},
+            {"jesus.herrera", "Mepiel2025"},
+            {"gerardo.reyes", "Mepiel2025"},
+            {"abisag.alcorta", "Mepiel2025"},
+            {"enrique.navarro", "Mepiel2025"},
+            {"jorge.barragan", "Mepiel2025"},
+            {"monica.urena", "Mepiel2025"},
         };
         public static Dictionary<string, string> DiccionarioNombres = new Dictionary<string, string>()
         {
-            {"ivan.lopez@dermaexpress.com.mx", "Ivan Lopez"},
-            {"jesus.herrera@dermaexpress.com.mx", "Jesus Herrera"}
+            {"ivan.lopez", "Ivan Lopez"},
+            {"jesus.herrera", "Jesus Herrera"},
+            {"gerardo.reyes", "Gerardo Reyes"},
+            {"abisag.alcorta", "Abisag Alcorta"},
+            {"enrique.navarro", "Enrique Navarro"},
+            {"jorge.barragan", "Jorge Barragan"},
+            {"monica.urena", "Monica Ureña"},
         };
     }
 }
--- assets/js/config.js
+++ assets/js/config.js
@@ -25,7 +25,7 @@ const cleanClass = () => {
  
 // Verificar la ruta y agregar el 'active' solo al enlace correspondiente
 if (currentPath.includes('/home/index') || currentPath.includes('/Home/Index') || currentPath.includes("/Home/index")) {  
-    textHeader.textContent = "Administraci\u00f3n del Reporte" 
+    textHeader.textContent = "Reporte CEDIS" 
     textDescription.textContent = "En este modulo podr\u00e1s visualizar el reporte generado por el aplicativo."
 
     reportLink.classList.add('active');
